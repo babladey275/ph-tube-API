e77bacec4a5dcbb6761e0f06362fc84aa5fdfd14
babladey275 <babladey275@gmail.com>
2024-10-03 11:48:55 +0600

    added navbar and categories fetch promises complete

---
+++ scripts/video.js
@@ -0,0 +1,24 @@
+/**
+ * 1. fetch, load and show categories on html
+ * 
+ */
+
+const loadCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(error => console.log(error));
+}
+
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('categories');
+    categories.forEach(item => {
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerText = item.category;
+
+        categoryContainer.appendChild(button);
+    });
+}
+
+loadCategories();
